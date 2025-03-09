77dca1a21ea697eb4b8cf36695f822d70703a77c
Mattia Delaini <148365360+masteroid007@users.noreply.github.com>
2025-03-09 18:10:21 +0100

    Delete home directory

--- home/button.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var contactButton = document.getElementById('01');
-  if (contactButton) {
-    contactButton.addEventListener('click', function() {
-      window.location.href = 'mailto:mattia@masterwork.me';
-    });
-  }
-});
